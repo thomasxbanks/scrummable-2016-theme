8a1c41e56fdf3a69258e74c302eb0d185e9a13d6
thombanks <thomas.banks@wearecube3.com>
2017-06-29 13:49:11 +0100

    new brand color styling

--- partials-post/teaser/post-content.php
+++ partials-post/teaser/post-content.php
@@ -2,8 +2,6 @@
     <?php get_template_part('partials-post/post', 'meta'); ?>
     <section class="post__content--inner">
         <?php echo (has_excerpt()) ? the_excerpt() : paragraph_excerpt(get_the_content()); ?>
-        <a class="button button--big" href="<?php the_permalink(); ?>" title="Read more of &ldquo;<?php the_title(); ?>&rdquo;">
-            Read more
-        </a>
+        <a class="read-more" href="<?php the_permalink(); ?>" title="Read more of &ldquo;<?php the_title(); ?>&rdquo;">Read more <span class="arrow">&rsaquo;</span><span class="underline" style="box-shadow: inset 0 -4px 0  #${post.color};"></span></a>
     </section>
 </div>
