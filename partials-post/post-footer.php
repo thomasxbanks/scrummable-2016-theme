35f75019a555484ebbaaef9c6ca4e1b5a48a3e1d
thombanks <thomas.banks@wearecube3.com>
2017-07-03 13:35:26 +0100

    bugs - fixed single post hero

--- partials-post/post-footer.php
+++ partials-post/post-footer.php
@@ -3,7 +3,7 @@
 $the_category = get_the_category_list();
 $the_tags = get_the_tag_list();
 ?>
-<footer id="post__footer--<?php echo get_the_ID(); ?>" class="post__footer" data-role="post footer" aria-label="Post Footer">
+<footer id="post__footer--<?php echo get_the_ID(); ?>" class="post__footer post__content central-column" data-role="post footer" aria-label="Post Footer">
     <?php if (($the_category) || ($the_tags)) { ?>
         <?php ($the_category) ? get_template_part('partials-post/post', 'cats') : null; ?>
         <?php ($the_tags) ? get_template_part('partials-post/post', 'tags') : null; ?>
--- php/functions-post.php
+++ php/functions-post.php
@@ -27,15 +27,20 @@ function custom_post($variant) {
       default:
           $post_classes .= " item-4";
           break;
-  } ?>
+  }
+
+  ?>
   <article id="post--<?php the_ID(); ?>" class="<?php echo $post_classes ." ~ ". $count; ?>" itemscope itemtype="https://schema.org/Blog">
     <?php get_template_part('partials-post/' . $variant . '/post', 'hero'); ?>
       <div class="inner">
           <?php get_template_part('partials-post/' . $variant . '/post', 'content'); ?>
           <?php get_template_part('partials-post/post', 'footer'); ?>
-          <?php if (($variant == 'single') && (comments_open())) {
-      comments_template();
-  } ?>
+          <?php echo ($auth_bio = nl2br(get_the_author_meta('description'))) ? "<div class=\"post__content central-column\">" . $auth_bio . "</div><br />" : null; ?>
+          <?php if (($variant == 'single') && (comments_open())) { ?>
+            <div class="post__content central-column">
+              <?php comments_template(); ?>
+            </div>
+          <?php } ?>
       </div>
   </article>
 <?php }
