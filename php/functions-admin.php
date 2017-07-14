f5b96a8b986aa26ec7a78d5413dbc0e2a2affb15
thombanks <thomas.banks@wearecube3.com>
2017-07-14 13:37:40 +0100

    bug - CMS collapse nav issue

--- php/functions-admin.php
+++ php/functions-admin.php
@@ -44,6 +44,14 @@ function my_save_extra_profile_fields( $user_id ) {
  * Author URI: 		http://www.jeroensormani.com/
  */
 
+ function load_custom_wp_admin_script() {
+         wp_register_script( 'custom_wp_admin_script', get_template_directory_uri() . '/php/admin-script.js', false, '1.0.0' );
+         wp_enqueue_script( 'custom_wp_admin_script' );
+ }
+
+ add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_script' );
+
+
 add_action( 'admin_menu', 'register_media_selector_settings_page' );
 
 function register_media_selector_settings_page() {
@@ -59,8 +67,8 @@ function media_selector_settings_page_callback() {
 
 	wp_enqueue_media();
 
-	?><form method='post'>
-		<div class='image-preview-wrapper'>
+	?><form method='post' style='margin: 1rem;' data-hook="<?php echo $my_saved_attachment_post_id; ?>">
+		<div class='image-preview-wrapper' style='margin: 1rem 0;'>
 			<img id='image-preview' src='<?php echo wp_get_attachment_url( get_option( 'media_selector_attachment_id' ) ); ?>' height='100'>
 		</div>
 		<input id="upload_image_button" type="button" class="button" value="<?php _e( 'Upload image' ); ?>" />
@@ -77,63 +85,5 @@ function media_selector_print_scripts() {
 
 	$my_saved_attachment_post_id = get_option( 'media_selector_attachment_id', 0 );
 
-	?><script type='text/javascript'>
-
-		jQuery( document ).ready( function( $ ) {
-
-			// Uploading files
-			var file_frame;
-			var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
-			var set_to_post_id = <?php echo $my_saved_attachment_post_id; ?>; // Set this
-
-			jQuery('#upload_image_button').on('click', function( event ){
-
-				event.preventDefault();
-
-				// If the media frame already exists, reopen it.
-				if ( file_frame ) {
-					// Set the post ID to what we want
-					file_frame.uploader.uploader.param( 'post_id', set_to_post_id );
-					// Open frame
-					file_frame.open();
-					return;
-				} else {
-					// Set the wp.media post id so the uploader grabs the ID we want when initialised
-					wp.media.model.settings.post.id = set_to_post_id;
-				}
-
-				// Create the media frame.
-				file_frame = wp.media.frames.file_frame = wp.media({
-					title: 'Select a image to upload',
-					button: {
-						text: 'Use this image',
-					},
-					multiple: false	// Set to true to allow multiple files to be selected
-				});
-
-				// When an image is selected, run a callback.
-				file_frame.on( 'select', function() {
-					// We set multiple to false so only get one image from the uploader
-					attachment = file_frame.state().get('selection').first().toJSON();
-
-					// Do something with attachment.id and/or attachment.url here
-					$( '#image-preview' ).attr( 'src', attachment.url ).css( 'width', 'auto' );
-					$( '#image_attachment_id' ).val( attachment.id );
-
-					// Restore the main post ID
-					wp.media.model.settings.post.id = wp_media_post_id;
-				});
-
-					// Finally, open the modal
-					file_frame.open();
-			});
-
-			// Restore the main ID when the add media button is pressed
-			jQuery( 'a.add_media' ).on( 'click', function() {
-				wp.media.model.settings.post.id = wp_media_post_id;
-			});
-		});
-
-	</script><?php
 
 }
