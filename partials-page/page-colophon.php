63089090f0b3fc33005616533bfedb7b49e6b10a
thombanks <thomas.banks@wearecube3.com>
2017-08-07 16:50:52 +0100

    easter-egg

--- partials-page/page-colophon.php
+++ partials-page/page-colophon.php
@@ -4,4 +4,9 @@
 			&copy; <?php copyright_date(); ?> scrummable // <?php echo get_bloginfo('description'); ?>
 		</small>
 	</div>
+	<?php
+		if (microtime(true) %2 === 0) {
+		?><style>.easter-egg { color: rgba(0,0,0,0.3); float: right; text-decoration: none; } </style><a class="easter-egg" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=pXPXMxsXT28">π</a><?php
+		}
+	?>
 </footer>
