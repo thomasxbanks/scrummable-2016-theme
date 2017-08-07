24606a3f70a1713f8eaa56fe37dc2076f12f94e1
thombanks <thomas.banks@wearecube3.com>
2017-08-07 13:05:56 +0100

    new hook for return-to-top button

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<button id="return_to_top" class="button button--big" type="button" data-role="button" aria-label="Return to the top of the content" onClick="return_to_top();">
+<button id="return_to_top" class="button button--big" type="button" data-state="not-shown" data-role="button" aria-label="Return to the top of the content" onClick="return_to_top();">
     <img src="<?php echo get_template_directory_uri(); ?>/img/ui/up.svg" alt="Skip to content">
 </button>
 <?php get_template_part('partials-page/page', 'colophon'); ?>
