a581768aa4f3b7e03c845b08bc92ba813aa14bd0
thomasxbanks <thomasxbanks@gmail.com>
2017-11-25 15:16:13 +0000

    bug - return to top button in footer not showing

--- js/main.js
+++ js/main.js
@@ -200,9 +200,16 @@ window.onscroll = function () {
 		document.querySelector('.scroll-down').setAttribute('data-state', 'shown')
 	}
 
+// 	const measurements = () => {
+// return `document_height: ${document_height()}\n
+// scroll: ${scroll}\n
+// browser.height: ${browser.height}`
+// 	}
+	// console.log(measurements())
+
 	//console.log("doc_height: "+document_height+". \browser.height: "+browser.height+". \ndoc - screen: "+(document_height - (browser.height * 2))+". scroll: "+scroll)
 
-	if (scroll > (document_height - (browser.height * 2))) {
+	if (scroll > (document_height() - (browser.height * 2))) {
 		document.querySelector('button#return_to_top').setAttribute('data-state', 'is-shown')
 	} else {
 		document.querySelector('button#return_to_top').setAttribute('data-state', 'not-shown')
