af6993699541a1842a04bba0afa03932ecb2723b
thomasxbanks <thomasxbanks@gmail.com>
2018-01-31 13:43:49 +0000

    IPV - Lighthouse a11y test failed. Fixes in place.

--- header.php
+++ header.php
@@ -8,7 +8,7 @@ if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
 <?php get_template_part('partials-page/page', 'peeker'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="EN" xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta charset="UTF-8">
   <!-- pre-fetch the cdn for the googlefonts and analytics. Performance advantage -->
@@ -25,8 +25,8 @@ if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
 	<!--Uncomment this to use a favicon.ico in the theme directory: -->
 	<!--<link rel="SHORTCUT ICON" href="<?php bloginfo('template_directory'); ?>/favicon.ico"/>-->
 	<?php if (is_singular()) {
-    wp_enqueue_script('comment-reply');
-} ?>
+    	wp_enqueue_script('comment-reply');
+	} ?>
 	<?php wp_head(); ?>
 	<link href="https://fonts.googleapis.com/css?family=Comfortaa:300|Overpass:300" rel="stylesheet">
   <?php get_template_part('partials-page/page', 'meta'); ?>
--- partials-page/page-colophon.php
+++ partials-page/page-colophon.php
@@ -1,12 +1,7 @@
 <footer id="colophon" class="footer--page" data-role="page footer" aria-label="Page footer and colophon">
 	<div id="copyright">
 		<small>
-			&copy; <?php copyright_date(); ?> scrummable // <?php echo get_bloginfo('description'); ?>
+			&copy; <?php copyright_date(); ?> <?php echo get_bloginfo('name'); ?> // <?php echo get_bloginfo('description'); ?>
 		</small>
 	</div>
-	<?php
-		if (microtime(true) %2 === 0) {
-		?><style>.easter-egg { color: rgba(0,0,0,0.3); float: right; text-decoration: none; } </style><a class="easter-egg" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=pXPXMxsXT28">π</a><?php
-		}
-	?>
 </footer>
