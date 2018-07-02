a0b10022a47184c0c2d38ca4d928a7c4931d8616
thomasxbanks <thomasxbanks@gmail.com>
2018-07-02 09:01:50 +0100

    SCR-15 - favicons, app icons, and manifest

--- header.php
+++ header.php
@@ -14,6 +14,20 @@ if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
   <link rel="dns-prefetch" href="//fonts.googleapis.com">
   <link rel="dns-prefetch" href="//google-analytics.com">
   <link rel="dns-prefetch" href="//www.google-analytics.com">
+	<!-- Favicons, app icons, and manifest -->
+	
+
+<link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
+<link rel="manifest" href="/img/favicons/site.webmanifest">
+<link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#a1213d">
+<link rel="shortcut icon" href="/img/favicons/favicon.ico">
+<meta name="msapplication-TileColor" content="#a1213d">
+<meta name="msapplication-config" content="/img/favicons/browserconfig.xml">
+<meta name="theme-color" content="#a1213d">
+
+
 	<title>
 		<?php wp_title(' | ', true, 'right'); ?>
 		<?php bloginfo('name'); ?>
